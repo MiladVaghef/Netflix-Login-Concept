0c9236877c7df6bb58f18d00db054e592611acb5
Chef Mila <miladvaghefdev@gmail.com>
2024-06-20 09:13:14 +0330

    Refactor: Dynamic banner section
    Here I have coded the banner section using the functions from utility.js
    
    This file was supposed to added on previous commit but I missed it

---
+++ Components/banner.js
@@ -0,0 +1,37 @@
+// Imported functions from utility
+import {
+  createElementsWithID, // create HTML elements with ID
+  createEelementsWithClassAndID, // create html elements with class and ID
+  appendChild, // appendChild elements
+  createElement, // create elements without ID and class
+  addImage, // create and add link to created image
+} from "../utility.js";
+
+export function banner() {
+  // create the banner section element
+  let bannerSection = createEelementsWithClassAndID(
+    "section",
+    "netflixBanner",
+    "column",
+    "main"
+  );
+
+  // create the banner image element
+  let bannerImage = createElementsWithID("div", "bannerImage");
+  appendChild(bannerSection, bannerImage);
+  // add the netflix TV image
+  addImage("Images/Netflix-TV.svg", bannerImage).setAttribute(
+    "alt",
+    "NetflixBanner"
+  );
+
+  // create the banner text element
+  let bannerText = createElementsWithID("div", "bannerText");
+  appendChild(bannerSection, bannerText);
+  // create the "Ready To Watch" title
+  let readyToWatch = createElement("h2", bannerText);
+  readyToWatch.innerHTML = "Ready To<br> Watch";
+
+  // return the completed banner section
+  return secondSection;
+}
